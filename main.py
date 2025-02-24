105db278ae8944e2d1457afd32d564bdb5abde4b
Sergiox098 <segomezco@unal.edu.co>
2025-02-24 14:30:40 -0500

    Cambiolen

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ varpais = input("Ingrese el país que desee consultar la cantidad de compras: ")
 import csv
 with open("C:/Users/sergio gomez/Documents/Programación 2024/Archivos/files/files/SalesJan2009.csv",newline="") as f:
     a=list((csv.DictReader(f)))
-    pais = [col8["Country"] for col8 in a]
-    producto = [col8["Country"] for col8 in a]
+    pais = [col["Country"] for col in a]
+    producto = [col["Product"] for col in a]
     lista = []
     ilista = []
     b = []
@@ -14,5 +14,10 @@ with open("C:/Users/sergio gomez/Documents/Programación 2024/Archivos/files/fil
             pais.remove(i)
     for i in ilista:
         b.append(producto[i-1])
-sumproduct = len(b)   
+    valorproducto = []
+    for i in b:
+        x = i.replace("Product","")
+        valorproducto.append(x)   
+print(valorproducto)
+sumproduct =sum(map(int,valorproducto))
 print("El país \"",varpais,"\" en el 2009 tuvo un total de",sumproduct,"compras realizadas")
